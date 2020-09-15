59f75090b03dbbc0dcd4ef0a9004c4bf5f8e5da4
Dipendra Bahadur Chand <chand.dipendra19@gmail.com>
2020-09-16 01:27:35 +0545

    dynamic userId

---
+++ public/script.js
@@ -0,0 +1,31 @@
+const socket = io('/')
+const videoGrid = document.getElementById('video-grid')
+const myPeer = new Peer(undefined, {
+    host: '/',
+    port: '3001'
+})
+const myVideo = document.createElement('video')
+myVideo.muted = true
+
+navigator.mediaDevices.getUserMedia({
+    video: true,
+    audio: true
+}).then(stream => {
+
+})
+
+myPeer.on('open', id => {
+    socket.emit('join-room', ROOM_ID, id);
+})
+
+socket.on('user-connected', userId => {
+    console.log('User Connected: ' + userId)
+})
+
+function addVideoStream(video, stream) {
+    video.srcObject = stream
+    video.addEventListener('loadedmetadata', () => {
+        video.play()
+    })
+    videoGrid.append(video)
+}
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const { emit } = require('process');
 const app = express();
 const server = require('http').Server(app);
 const io = require('socket.io')(server);
@@ -13,6 +14,14 @@ app.get('/', (req, res) => {
 
 app.get('/:room', (req, res) => {
     res.render('room', { roomId: req.params.room })
+})
+
+io.on('connection', socket => {
+    socket.on('join-room', (roomId, userId) => {
+        //console.log(roomId, userId)
+        socket.join(roomId)
+        socket.to(roomId).broadcast.emit('user-connected', userId)
+    })
 })
 
 server.listen(3000);
